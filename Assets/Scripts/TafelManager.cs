1d2926b175f0326edd8f5412e823c65892e119d9
AgeOfTheJoshua <61249596+AgeOfTheJoshua@users.noreply.github.com>
2026-05-05 00:06:51 +0200

    CanGame win effects

--- Assets/Scripts/TafelManager.cs
+++ Assets/Scripts/TafelManager.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 using TMPro;
+using FMODUnity;
 
 public class TafelManager : MonoBehaviour
 {
-    [SerializeField]
-    TMP_Text dosenToClearText;
+    [SerializeField] TMP_Text dosenToClearText;
+    [SerializeField] private StudioEventEmitter scoreBoardScribbleSound;
+    [SerializeField] private StudioEventEmitter canGameWinSound;
+    [SerializeField] private ParticleSystem canGameWinPS;
+    private int lastCount = -1; 
 
     public void SetDosenToClear(int count)
     {
+        if (count == lastCount) return;
+        lastCount = count;
+        scoreBoardScribbleSound.Play();
         dosenToClearText.text = count.ToString();
+        if (count == 0) {
+            canGameWinSound.Play();
+            canGameWinPS.Play();
+        }
     }
 }
